91383957246b0ef93d981604562fc70cabd14eb8
unknown <yeshratzerinyera@gmail.com>
2022-12-05 00:57:32 +0600

    Data Error Fixed

--- index.js
+++ index.js
@@ -12,25 +12,25 @@ app.get('/', (req, res) => {
     res.send('Xplore API Running');
 });
 
-// app.get('/categories', (req, res) => {
-//     res.send(categories);
-// });
+app.get('/categories', (req, res) => {
+    res.send(categories);
+});
 
-// app.get('/categories/:id', (req, res) => {
-//     const id = req.params.id;
-//     const categoryCourses = courses.filter(course => course.category_id === id);
-//     res.send(categoryCourses);
-// });
+app.get('/categories/:id', (req, res) => {
+    const id = req.params.id;
+    const categoryCourses = courses.filter(course => course.category_id === id);
+    res.send(categoryCourses);
+});
 
-// app.get('/courses', (req, res) => {
-//     res.send(courses);
-// });
+app.get('/courses', (req, res) => {
+    res.send(courses);
+});
 
-// app.get('/courses/:id', (req, res) => {
-//     const id = req.params.id;
-//     const selectedCourse = courses.find(course => course._id === id);
-//     res.send(selectedCourse);
-// });
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courses.find(course => course._id === id);
+    res.send(selectedCourse);
+});
 
 
 app.listen(port, () => {
