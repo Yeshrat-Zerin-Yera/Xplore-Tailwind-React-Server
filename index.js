88637139aff72cdadcc309f53989e8ab574403a5
unknown <yeshratzerinyera@gmail.com>
2022-12-05 00:02:09 +0600

    Git Ignore Added

--- index.js
+++ index.js
@@ -16,21 +16,21 @@ app.get('/categories', (req, res) => {
     res.send(categories);
 });
 
-app.get('/categories/:id', (req, res) => {
-    const id = req.params.id;
-    const categoryCourses = courses.filter(course => course.category_id === id);
-    res.send(categoryCourses);
-});
+// app.get('/categories/:id', (req, res) => {
+//     const id = req.params.id;
+//     const categoryCourses = courses.filter(course => course.category_id === id);
+//     res.send(categoryCourses);
+// });
 
-app.get('/courses', (req, res) => {
-    res.send(courses);
-});
+// app.get('/courses', (req, res) => {
+//     res.send(courses);
+// });
 
-app.get('/courses/:id', (req, res) => {
-    const id = req.params.id;
-    const selectedCourse = courses.find(course => course._id === id);
-    res.send(selectedCourse);
-});
+// app.get('/courses/:id', (req, res) => {
+//     const id = req.params.id;
+//     const selectedCourse = courses.find(course => course._id === id);
+//     res.send(selectedCourse);
+// });
 
 
 app.listen(port, () => {
