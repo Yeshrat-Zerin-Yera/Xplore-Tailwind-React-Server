12f9942be372fad24eefcc6d700fbc411a097b98
unknown <yeshratzerinyera@gmail.com>
2022-12-06 00:26:51 +0600

    Blogs Data And Comments Added

--- index.js
+++ index.js
@@ -3,19 +3,24 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 
+// Get Data
 const categories = require('./data/categories.json');
 const courses = require('./data/courses.json');
+const blogs = require('./data/blogs.json');
 
+// Cors
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Xplore API Running');
 });
 
+// Categories
 app.get('/categories', (req, res) => {
     res.send(categories);
 });
 
+// Categories ID
 app.get('/categories/:id', (req, res) => {
     const id = req.params.id;
     if (id === '01') {
@@ -34,26 +39,44 @@ app.get('/categories/:id', (req, res) => {
         const categoryCourses = courses.filter(course => course?.rating?.badge === 'Special');
         res.send(categoryCourses);
     }
+    else if (id === '09') {
+        res.send(courses);
+    }
     else {
         const categoryCourses = courses.filter(course => course?.category_id === id);
         res.send(categoryCourses);
     }
 });
 
+// Courses
 app.get('/courses', (req, res) => {
     res.send(courses);
 });
 
+// Courses ID
 app.get('/courses/:id', (req, res) => {
     const id = req.params.id;
     const selectedCourse = courses.find(course => course?._id === id);
     res.send(selectedCourse);
 });
 
+// Checkout ID
 app.get('/checkout/:id', (req, res) => {
     const id = req.params.id;
     const selectedCourse = courses.find(course => course?._id === id);
     res.send(selectedCourse);
+});
+
+// Blogs
+app.get('/blogs', (req, res) => {
+    res.send(blogs);
+});
+
+// Blogs ID
+app.get('/checkout/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedBlog = blogs.find(blog => blog?._id === id);
+    res.send(selectedBlog);
 });
 
 
